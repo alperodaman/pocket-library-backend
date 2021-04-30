4e849b4303af21726cf9054dd43f2a99ed5ee9a8
Alper Odaman <alperodaman@gmail.com>
2021-04-30 14:01:03 +0300

    Delete unnecesarry lines

--- api/utils/generateCode.js
+++ api/utils/generateCode.js
@@ -1,7 +1,10 @@
-const genereateCode = () => {
-  let code = Math.floor(Math.random() * Math.floor(8999)) + 1000;
-  code = code.toString();
-  return code;
-};
+const genereateCode = () =>
+  (Math.floor(Math.random() * Math.floor(8999)) + 1000).toString();
+
+// const genereateCode = () => {
+//   let code = Math.floor(Math.random() * Math.floor(8999)) + 1000;
+//   code = code.toString();
+//   return code;
+// };
 
 export { genereateCode };
