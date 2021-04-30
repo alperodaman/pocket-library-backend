851cc47dc59df5c445d5c353f180abe4b0fd4e01
Alper Odaman <alperodaman@gmail.com>
2021-04-30 14:01:34 +0300

    Delete unnecesarry lines

--- api/utils/generateCode.js
+++ api/utils/generateCode.js
@@ -1,10 +1,4 @@
 const genereateCode = () =>
   (Math.floor(Math.random() * Math.floor(8999)) + 1000).toString();
 
-// const genereateCode = () => {
-//   let code = Math.floor(Math.random() * Math.floor(8999)) + 1000;
-//   code = code.toString();
-//   return code;
-// };
-
 export { genereateCode };
--- server.js
+++ server.js
@@ -2,14 +2,12 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-// import bodyParser from "body-parser";
 import router from "./api/router/index.js";
 import "./api/utils/db.js";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use(bodyParser.json());
 app.use("/", router);
 
 const PORT = process.env.PORT || 8000;
